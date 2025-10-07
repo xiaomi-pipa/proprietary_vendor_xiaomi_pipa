f6d9c4e9693fd2e70b3b4fae5c127ef633abdd76
pexcn <pexcn97@gmail.com>
2025-10-08 00:18:33 +0800

    pipa: Remove firmware blobs

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,22 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    bluetooth \
-    cmnlib \
-    cmnlib64 \
-    devcfg \
-    dsp \
-    featenabler \
-    hyp \
-    imagefv \
-    keymaster \
-    modem \
-    qupfw \
-    tz \
-    uefisecapp \
-    xbl \
-    xbl_config
